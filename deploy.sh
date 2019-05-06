c36bf7e9df18fe067b891b52aef6620fe98dd6e0
Igor Susmelj <isusmelj@gmail.com>
2019-05-06 14:16:33 +0200

    Add `deploy.sh` script
    This script builds, tags and uploads a container to google cloud.
    
    Signed-off-by: Igor Susmelj isusmelj@gmail.com

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+rocket_slug="lucas/yolov3"
+tag="0.2"
+
+# build the container
+docker build --tag "$rocket_slug:$tag" .
+
+# tag the container
+docker tag "$rocket_slug:$tag" "gcr.io/rockethub/$rocket_slug:$tag"
+
+# upload the container
+docker push "gcr.io/rockethub/$rocket_slug:$tag"
+
